a4ac61db3ef596f36fbdc44fa00c93c1066d10cd
ParchedPanda <tekic.ana@gmail.com>
2020-06-09 22:30:23 +0200

    final game

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
-
 function computerPlay () { //choose a random computer choice
   let myArray = ["Rock", "Paper", "Scissors"]
   return  myArray[Math.floor(Math.random()*myArray.length)];
   }
 
-function humanPlay() { //valja kad se testira. console log vrati kliknut paper ili rock ili scissors
+function humanPlay() { 
      return(event.target.id)
 }
 
@@ -14,99 +13,115 @@ document.getElementById('Paper').addEventListener('click', playRound)
 document.getElementById('Scissors').addEventListener('click', playRound)
 
 let playerScore =0;
- let computerScore= 0;
+let computerScore= 0;
+let log = document.getElementById("logs");
+let roundResult = document.getElementById("roundResult");
+let declareWin = document.getElementById("declareWin");
 
-function game() {
- 
-}
-  
-  function playRound(playerSelection, computerSelection) {
+
+function playRound(playerSelection, computerSelection) {
       computerSelection = computerPlay();
       playerSelection = humanPlay();
      
- 
- 
     if (computerSelection==playerSelection) {
       playerScore++;
       computerScore++;
-      document.getElementById("logs").innerText = `Computer chose ${computerSelection}. 
-Player chose ${playerSelection}. 
-It's a tie!`;
-       
-   }
+
+      log.innerText = `Computer chose ${computerSelection}. 
+      Player chose ${playerSelection}. `;
+      
+      roundResult.innerText = `It's a tie`;
+     
+      }
+
      else if(playerSelection=="Paper" && computerSelection=="Rock" ||
               playerSelection =="Rock" && computerSelection=="Scissors" ||
               playerSelection=="Scissors" && computerSelection=="Paper") {
       playerScore++;
        
-      document.getElementById("logs").innerText = `Computer chose ${computerSelection}. 
-Player chose ${playerSelection}. 
-Player wins!`;
-         
+      log.innerText = `Computer chose ${computerSelection}. 
+      Player chose ${playerSelection}. `;
+      roundResult.innerText = `Player wins!`  ;
     }
                 
      else if(computerSelection=="Scissors" && playerSelection=="Paper" ||
              computerSelection=="Rock" && playerSelection=="Scissors" ||
-              computerSelection =="Paper" && playerSelection=="Rock") {
-     computerScore++;
-           document.getElementById("logs").innerText = `Computer chose ${computerSelection}. 
-Player chose ${playerSelection}. 
-Computer wins!`;
-         
-     };
-     document.getElementById("counter").innerText = `Player ${playerScore} : ${computerScore} Computer`;
+             computerSelection =="Paper" && playerSelection=="Rock") {
+           
+              computerScore++;
+
+            log.innerText = `Computer chose ${computerSelection}. 
+            Player chose ${playerSelection}`;
+            roundResult.innerText = `Computer wins!`;
+             };
+
+  document.getElementById("counter").innerText = `Player ${playerScore} : ${computerScore} Computer`;
   
 
  if(playerScore==5 || computerScore==5) {
-
+ roundResult.innerText= "";
+ document.getElementById('Rock').removeEventListener('click', playRound) 
+document.getElementById('Paper').removeEventListener('click', playRound)
+document.getElementById('Scissors').removeEventListener('click', playRound)
   if (computerScore>playerScore) { 
-    document.getElementById("logs").innerText= "GAME OVER.Machines rule";
+    declareWin.innerText= "GAME OVER. Machines rule.";
   }
   else if (playerScore>computerScore){
-    document.getElementById("logs").innerText="GAME OVER. HUMANS ARE SUPERIOR";
+    declareWin.innerText="GAME OVER. HUMANS ARE SUPERIOR";
   }
     else {
-   document.getElementById("logs").innerText="GAME OVER. HUMAN EQUAL MACHINE?"
+    declareWin.innerText="GAME OVER. HUMAN EQUAL MACHINE?"
     }
+
    playAgain();
    }
 
-    
- function playAgain() {
-     document.getElementById("logs").innerText= "Play again?";
- 
-     var btnYes = document.createElement("BUTTON");
+  }
+
+function playAgain() {
+  let playAgain = document.getElementById("playAgain");
+      playAgain.innerText= "Play again?";
+      var btnYes = document.createElement("BUTTON");
      btnYes.innerHTML = "YES";
-             
-logs.appendChild(btnYes);  
-     
+     playAgain.appendChild(btnYes);  
      var btnNo = document.createElement("BUTTON");   
-  
      btnNo.innerHTML = "NO";                  
-logs.appendChild(btnNo);
+     playAgain.appendChild(btnNo);
    
-   btnYes.addEventListener('click', function restart(){playerScore=0; computerScore=0; logs.innerHTML = ""; playRound()});
+   btnYes.addEventListener('click', restart);
    btnNo.addEventListener ('click', quitGame);
+ }
 
- 
-  }}
-
-  
+  function restart () {
+    document.getElementById('Rock').addEventListener('click', playRound) 
+    document.getElementById('Paper').addEventListener('click', playRound)
+    document.getElementById('Scissors').addEventListener('click', playRound)
+    playerScore=0; 
+    computerScore=0; 
+    document.getElementById("counter").innerText = `Player ${playerScore} : ${computerScore} Computer`;
+    log.innerHTML = ""; 
+    declareWin.innerHTML= "";
+    document.getElementById("playAgain").innerText= "";
+  }
+   
 
   function quitGame() {
-    document.getElementById("logs").innerText="No more game.";
-     
+    log.innerText="No more game.";
+    declareWin.innerHTML= "";
     document.getElementById("Rock").onclick = function() {
-      window.location.href = 'https://www.nationalgeographic.com/science/earth/inside-the-earth/rocks/';
+      window.location.href= 'https://www.vampiretools.com/blog/short-history-scissors/' ;
     };
     document.getElementById("Paper").onclick= function(){
-        window.location.href= 'https://interestingengineering.com/the-long-and-complex-history-of-paper';
-    
-      }   ;
+      window.location.href= 'https://www.vampiretools.com/blog/short-history-scissors/';
+      }  
     document.getElementById("Scissors").onclick= function () {
-        window.location.href= 'https://www.vampiretools.com/blog/short-history-scissors/';
+      window.location.href= 'https://www.vampiretools.com/blog/short-history-scissors/'
+       ;
   };
-   
+  
+
   }
+     
+  
   
 
